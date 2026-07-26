0f335c7769f0c6371e8f69baa439fbcac12fbd18
Kittithad-Ittipon <a.kittithad.ittipon@gmail.com>
2026-07-26 15:53:20 +0700

    fix: footer link

--- adocs-frontend/components/Footer.tsx
+++ adocs-frontend/components/Footer.tsx
@@ -6,7 +6,7 @@ import { SiGmail } from "react-icons/si";
 export default function Footer() {
   return (
     <footer className="w-full h-30 bg-black/90 dark:bg-[oklch(0.2_0_0)] flex flex-col justify-start items-center p-5 gap-6">
-      <div className="w-full flex justify-center items-center gap-6">
+      {/* <div className="w-full flex justify-center items-center gap-6">
         <a
           href="https://www.facebook.com/a.kittihad"
           target="_blank"
@@ -21,7 +21,7 @@ export default function Footer() {
         >
           <SiGmail className="text-white cursor-pointer h-6 w-6 hover:text-red-400 transition duration-200"/>
         </a>
-      </div>
+      </div> */}
       <div className="text-white flex justify-center items-center text-[10px] md:text-sm xl:text-sm xl:font-[500] gap-3">
         <p>2.1.0</p>
         <FaRegCopyright className="text-white cursor-pointer h-4 w-4" />
